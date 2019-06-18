124df40b1496c843e8e101d9742370b53aa6307f
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-18 17:42:16 +0200

    Increment update operator

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update a user out of the database', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -40,13 +40,16 @@ describe('Update a user out of the database', () => {
     });
 
     it('a model class can find a record with an ID and update', (done) => {
-        assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex'}), done);
-        
+        assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex'}), done);   
     });
 
-    it('class method ', (done) => {
-        done();
-        
+    it('A user can have their postcount incremented by 1 ', (done) => {
+        User.updateMany({ name: 'Joe'}, { $inc: {postCount: 1}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            })
     });
 
     it('class method ', (done) => {
