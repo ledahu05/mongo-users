2113e676fa071bf83b51a2f79a075667b9971765
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-19 15:50:42 +0200

    fix update test

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update a user out of the database', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe', postCount: 0});
+        joe = new User({name: 'Joe', likes: 0});
         joe.save()
             .then(() => done());
     });
@@ -43,11 +43,11 @@ describe('Update a user out of the database', () => {
         assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex'}), done);   
     });
 
-    xit('A user can have their postcount incremented by 1 ', (done) => {
-        User.updateMany({ name: 'Joe'}, { $inc: {postCount: 1}})
+    it('A user can have their postcount incremented by 1 ', (done) => {
+        User.updateMany({ name: 'Joe'}, { $inc: {likes: 1}})
             .then(() => User.findOne({name: 'Joe'}))
             .then((user) => {
-                assert(user.postCount === 1);
+                assert(user.likes === 1);
                 done();
             })
     });
