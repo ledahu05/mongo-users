dca904ebf4cb50f03102944fa7c16a131eebb2a0
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-19 15:25:02 +0200

    removing subdocuments

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -34,4 +34,23 @@ describe('Subdocuments records', () => {
                 done();
             })              
     });
+
+    it('can remove an existing subdocument', (done) => {
+        const joe = new User({
+            name: 'Joe', 
+            posts: [{title: 'New Title'}]
+        });
+   
+        joe.save()
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                user.posts[0].remove();
+                return user.save();
+            })
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.posts.length === 0);
+                done();
+            })              
+    });
 });
