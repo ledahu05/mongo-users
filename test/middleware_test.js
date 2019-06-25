e9808a8b4efed344c37035942e0c538d81e01808
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-25 17:09:25 +0200

    skip and limit

--- test/middleware_test.js
+++ test/middleware_test.js
@@ -20,7 +20,7 @@ describe('Middleware', () => {
 
     it('users clean up dangling blogposts on remove', (done) => {
         joe.remove()
-            .then(() => BlogPost.count())
+            .then(() => BlogPost.countDocuments())
             .then((count) => {
                 assert(count === 0)
                 done();
